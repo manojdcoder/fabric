eff1bf49a2f42204a594bef2b7bf936ca0a67af0
Manoj Kumar <mano.mykingdom@gmail.com>
2015-06-13 23:43:34 +0530

    git ignore clean

--- iphone/build/crashlytics.build/Release-iphonesimulator/crashlytics.build/Objects-normal/x86_64/TiCrashlyticsModuleAssets.d
+++
@@ -1,3 +0,0 @@
-dependencies: \
-  /Users/MyKingdom/Documents/Appcelerator_Studio_Workspace/crashlytics/iphone/Classes/TiCrashlyticsModuleAssets.m \
-  /Users/MyKingdom/Documents/Appcelerator_Studio_Workspace/crashlytics/iphone/Classes/TiCrashlyticsModuleAssets.h
