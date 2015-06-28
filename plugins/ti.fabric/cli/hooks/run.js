ee92284f4dd199d9e1219d090560cbc519d4baaf
Manoj Kumar <mano.mykingdom@gmail.com>
2015-06-28 22:05:00 +0530

    plugin for android

--- plugins/ti.fabric/cli/hooks/run.js
+++ plugins/ti.fabric/cli/hooks/run.js
@@ -11,48 +11,93 @@ exports.init = function(logger, config, cli, appc) {
 
 	logger.info(TAG + ' : initiated');
 
-	var isProd = (cli.argv['deploy-type'] || cli.argv['deployment-type']) === 'production',
-	    projectDir = cli.argv['project-dir'];
+	var isProd = (cli.argv['deploy-type'] || cli.argv['deployment-type']) === 'production';
 
 	if (isProd || cli.argv['fabric-enabled'] === 'true') {
 
 		logger.info(TAG + ' : ' + ' initialization enabled');
 
-		cli.on('build.ios.xcodebuild', {
+		var projectDir = cli.argv['project-dir'];
 
-			pre : function(build, done) {
+		if (cli.argv['platform'] == 'ios') {
 
-				try {
+			var modules = cli.tiapp.modules;
+			for (var i in modules) {
+				var module = modules[i];
+				if (module.id == 'ti.fabric') {
+					VERSION = module.version;
+					break;
+				}
+			}
 
-					var path = projectDir + '/build/iphone/' + cli.tiapp.name + '.xcodeproj/project.pbxproj',
-					    proj = fs.readFileSync(path).toString(),
-					    sectionName = 'Post-Compile',
-					    shellScript = '\\nchmod 755 ../../modules/iphone/ti.fabric/' + VERSION + '/platform/Fabric.framework/run\\n../../modules/iphone/ti.fabric/1.0/platform/Fabric.framework/run ' + (cli.argv['fabric-key'] || API_KEY) + ' ' + (cli.argv['fabric-secret'] || API_SECRET);
+			cli.on('build.ios.xcodebuild', {
 
-					var p = 0;
-					while (p !== -1) {
-						p = proj.indexOf('name = "' + sectionName + '"', p);
-						if (p !== -1) {
-							p = proj.indexOf('shellScript = ', p);
+				pre : function(build, done) {
+
+					try {
+
+						logger.debug(TAG + ' : ' + ' processing fabric for ios');
+
+						var pbxprojPath = projectDir + '/build/iphone/' + cli.tiapp.name + '.xcodeproj/project.pbxproj',
+						    pbxprojStr = fs.readFileSync(pbxprojPath).toString(),
+						    sectionName = 'Post-Compile',
+						    shellScript = '\\nchmod 755 ../../modules/iphone/ti.fabric/' + VERSION + '/platform/Fabric.framework/run\\n../../modules/iphone/ti.fabric/1.0/platform/Fabric.framework/run ' + (cli.argv['fabric-key'] || API_KEY) + ' ' + (cli.argv['fabric-secret'] || API_SECRET);
+
+						var p = 0;
+						while (p !== -1) {
+							p = pbxprojStr.indexOf('name = "' + sectionName + '"', p);
 							if (p !== -1) {
-								proj = proj.substring(0, p) + 'shellScript = "' + proj.substring(p + 'shellScript = '.length + 1, proj.indexOf('\n', p) - 2) + shellScript + '";' + proj.substring(proj.indexOf('\n', p));
+								p = pbxprojStr.indexOf('shellScript = ', p);
+								if (p !== -1) {
+									pbxprojStr = pbxprojStr.substring(0, p) + 'shellScript = "' + pbxprojStr.substring(p + 'shellScript = '.length + 1, pbxprojStr.indexOf('\n', p) - 2) + shellScript + '";' + pbxprojStr.substring(pbxprojStr.indexOf('\n', p));
+								}
 							}
 						}
+
+						fs.writeFileSync(pbxprojPath, pbxprojStr);
+
+						done();
+
+					} catch(e) {
+
+						logger.error(TAG + ' : ' + e);
+						return;
+
 					}
 
-					fs.writeFileSync(path, proj);
-
-					done();
-
-				} catch(e) {
-
-					logger.error(TAG + ' : ' + e);
-					return;
-
 				}
+			});
 
-			}
-		});
+		} else if (cli.argv['platform'] == 'android') {
+
+			cli.on('build.android.writeAndroidManifest', {
+
+				post : function(build, done) {
+
+					try {
+
+						logger.debug(TAG + ' : ' + ' processing fabric for android');
+
+						var buildDir = projectDir + '/build/android/',
+						    srcCustomRules = projectDir + '/plugins/ti.fabric/android/custom_rules.xml',
+						    srcCrashlyticsFld = projectDir + '/plugins/ti.fabric/android/crashlytics';
+
+						fs.writeFileSync(buildDir + 'custom_rules.xml', fs.readFileSync(srcCustomRules).toString().replace("PROJECT_NAME", cli.tiapp.name));
+
+						require('fs-extra').copySync(srcCrashlyticsFld, buildDir + 'crashlytics');
+
+						done();
+
+					} catch(e) {
+
+						logger.error(TAG + ' : ' + e);
+						return;
+
+					}
+				}
+			});
+
+		}
 
 	}
 
